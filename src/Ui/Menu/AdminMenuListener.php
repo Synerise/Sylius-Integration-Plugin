774328bc69724f1e8b0318f259dccebbb8a12c96
Bartosz Sieczka <bartosz.sieczka@synerise.com>
2025-02-07 18:40:11 +0100

    Synerise menu

---
+++ src/Ui/Menu/AdminMenuListener.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Synerise\SyliusIntegrationPlugin\Ui\Menu;
+
+use Knp\Menu\ItemInterface;
+use Sylius\Bundle\UiBundle\Menu\Event\MenuBuilderEvent;
+
+final class AdminMenuListener
+{
+    public function __invoke(MenuBuilderEvent $event): void
+    {
+        $syneriseMenu = $event->getMenu()
+            ->addChild('synerise')
+            ->setLabel('Synerise')
+            ->setLabelAttribute('icon', 'tabler:currency-dollar');
+
+        $syneriseMenu
+            ->addChild('workspaces', ['route' => 'sylius_admin_product_index'])
+            ->setLabel('synerise_integration.ui.workspaces')
+            ->setLabelAttribute('icon', 'file')
+        ;
+
+        $menuChildren = $event->getMenu()->getChildren();
+        $marketingKey = array_search('marketing', array_keys($menuChildren)) ?: 4;
+        ++$marketingKey;
+        $menuChildren = array_slice($menuChildren, 0, $marketingKey, true) +
+            ['synerise' => $menuChildren['synerise']] +
+            array_slice($menuChildren, $marketingKey, (count($menuChildren) - 1) - $marketingKey, true);
+        $event->getMenu()->setChildren($menuChildren);
+    }
+}
