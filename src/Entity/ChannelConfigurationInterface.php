183454971b49922df5b30f7a22cff477b5c591a5
Bartosz Sieczka <bartosz.sieczka@synerise.com>
2025-03-17 14:07:33 +0100

    Tracking code template and configuration (#8)
    * Tracking code template and configuration
    
    * Configuration sections
    
    * Tracking code response update
    
    * Review

--- src/Entity/ChannelConfigurationInterface.php
+++ src/Entity/ChannelConfigurationInterface.php
@@ -16,4 +16,24 @@ interface ChannelConfigurationInterface extends ResourceInterface
     public function getWorkspace(): ?WorkspaceInterface;
 
     public function setWorkspace(?WorkspaceInterface $workspace): void;
+
+    public function isTrackingEnabled(): bool;
+
+    public function setTrackingEnabled(bool $trackingEnabled): void;
+
+    public function getTrackingCode(): ?string;
+
+    public function setTrackingCode(?string $trackingCode): void;
+
+    public function getCookieDomain(): ?string;
+
+    public function setCookieDomain(?string $cookieDomain): void;
+
+    public function isCustomPageVisit(): bool;
+
+    public function setCustomPageVisit(bool $customPageVisit): void;
+
+    public function isVirtualPage(): bool;
+
+    public function setVirtualPage(bool $virtualPage): void;
 }
--- src/Form/Type/ChannelConfigurationType.php
+++ src/Form/Type/ChannelConfigurationType.php
@@ -4,6 +4,8 @@ namespace Synerise\SyliusIntegrationPlugin\Form\Type;
 
 use Sylius\Bundle\ChannelBundle\Form\Type\ChannelChoiceType;
 use Sylius\Bundle\ResourceBundle\Form\Type\AbstractResourceType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 
 final class ChannelConfigurationType extends AbstractResourceType
@@ -12,11 +14,25 @@ final class ChannelConfigurationType extends AbstractResourceType
     {
         $builder
             ->add('channel', ChannelChoiceType::class, [
-                'label' => 'sylius.ui.channel',
-                'required' => false
+                'label' => 'sylius.ui.channel'
             ])
             ->add('workspace', WorkspaceChoiceType::class, [
-                'label' => 'synerise_integration.ui.workspace',
+                'label' => 'synerise_integration.ui.workspace'
+            ])
+            ->add('trackingEnabled', CheckboxType::class, [
+                'label' => 'synerise_integration.ui.tracking_enabled',
+                'required' => false
+            ])
+            ->add('cookieDomain', TextType::class, [
+                'label' => 'synerise_integration.ui.cookie_domain',
+                'required' => false
+            ])
+            ->add('customPageVisit', CheckboxType::class, [
+                'label' => 'synerise_integration.ui.custom_page_visit',
+                'required' => false
+            ])
+            ->add('virtualPage', CheckboxType::class, [
+                'label' => 'synerise_integration.ui.virtual_page',
                 'required' => false
             ])
         ;
