839561c42295b73cdba509f233c6b977a9f9d301
Bartosz Sieczka <bartosz.sieczka@synerise.com>
2025-05-20 16:42:14 +0200

    Migrations fix to match with sylius standard (#13)

--- src/Migrations/Version20250214153853.php
+++ src/Migrations/Version20250214153853.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace Synerise\SyliusIntegrationPlugin\Migrations;
 
 use Doctrine\DBAL\Schema\Schema;
-use Doctrine\Migrations\AbstractMigration;
+use Sylius\Bundle\CoreBundle\Doctrine\Migrations\AbstractMigration;
 
 /**
  * Auto-generated Migration: Please modify to your needs!
@@ -20,8 +20,8 @@ final class Version20250214153853 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE synerise_channel_configuration (id INT AUTO_INCREMENT NOT NULL, channel_id INT DEFAULT NULL, workspace_id INT DEFAULT NULL, UNIQUE INDEX UNIQ_8BF5FA0272F5A1AA (channel_id), INDEX IDX_8BF5FA0282D40A1F (workspace_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET UTF8 COLLATE `UTF8_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE synerise_workspace (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) DEFAULT NULL, apiKey BINARY(16) NOT NULL COMMENT \'(DC2Type:uuid)\', apiGuid CHAR(36) DEFAULT NULL COMMENT \'(DC2Type:guid)\', authenticationMethod VARCHAR(10) NOT NULL, environment VARCHAR(10) NOT NULL, permissions LONGTEXT DEFAULT NULL COMMENT \'(DC2Type:array)\', PRIMARY KEY(id)) DEFAULT CHARACTER SET UTF8 COLLATE `UTF8_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE synerise_channel_configuration (id INT AUTO_INCREMENT NOT NULL, channel_id INT DEFAULT NULL, workspace_id INT DEFAULT NULL, UNIQUE INDEX UNIQ_8BF5FA0272F5A1AA (channel_id), INDEX IDX_8BF5FA0282D40A1F (workspace_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE synerise_workspace (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) DEFAULT NULL, apiKey BINARY(16) NOT NULL COMMENT \'(DC2Type:uuid)\', apiGuid CHAR(36) DEFAULT NULL COMMENT \'(DC2Type:guid)\', authenticationMethod VARCHAR(10) NOT NULL, environment VARCHAR(10) NOT NULL, permissions LONGTEXT DEFAULT NULL COMMENT \'(DC2Type:array)\', PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE synerise_channel_configuration ADD CONSTRAINT FK_8BF5FA0272F5A1AA FOREIGN KEY (channel_id) REFERENCES sylius_channel (id)');
         $this->addSql('ALTER TABLE synerise_channel_configuration ADD CONSTRAINT FK_8BF5FA0282D40A1F FOREIGN KEY (workspace_id) REFERENCES sylius_channel (id)');
     }
