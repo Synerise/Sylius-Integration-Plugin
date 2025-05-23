9feb5ef5e8ef4ed17117fb147a452a7f6f761b9d
kamilkanik <kamil.kanik@icloud.com>
2025-05-23 12:21:11 +0200

    Changed Synchronization into SynchronizationConfiguration

---
+++ src/Entity/SynchronizationStatus.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Synerise\SyliusIntegrationPlugin\Entity;
+
+use Symfony\Contracts\Translation\TranslatableInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+enum SynchronizationStatus: string implements TranslatableInterface
+{
+    case Created = 'created';
+    case Processing = 'processing';
+    case Ended = 'ended';
+    case Cancelled = 'cancelled';
+
+    public const CREATED_LABEL = 'synerise_integration.synchronization.status.created';
+    public const PROCESSING_LABEL = 'synerise_integration.synchronization.status.processing';
+    public const ENDED_LABEL = 'synerise_integration.synchronization.status.ended';
+    public const CANCELLED_LABEL = 'synerise_integration.synchronization.status.cancelled';
+
+    public function trans(TranslatorInterface $translator, ?string $locale = null): string
+    {
+        return $translator->trans($this->getLabel(), locale: $locale);
+    }
+
+    public function getLabel(): string
+    {
+        return match ($this) {
+            self::Created  => self::CREATED_LABEL,
+            self::Processing => self::PROCESSING_LABEL,
+            self::Ended => self::ENDED_LABEL,
+            self::Cancelled => self::CANCELLED_LABEL,
+        };
+    }
+}
--- src/Form/Type/SynchronizationConfigurationType.php
+++ src/Form/Type/SynchronizationConfigurationType.php
@@ -6,7 +6,7 @@ use Sylius\Bundle\ChannelBundle\Form\Type\ChannelChoiceType;
 use Sylius\Bundle\ResourceBundle\Form\Type\AbstractResourceType;
 use Symfony\Component\Form\FormBuilderInterface;
 
-final class SynchronizationType extends AbstractResourceType
+final class SynchronizationConfigurationType extends AbstractResourceType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
