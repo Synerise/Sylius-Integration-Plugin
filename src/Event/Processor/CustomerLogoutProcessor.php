bacda69c4a183d577757fb97d32aaf803304c016
Bartosz Sieczka <bartosz.sieczka@synerise.com>
2025-06-02 10:32:44 +0200

    Further clean up

--- src/Event/Processor/CustomerLogoutProcessor.php
+++ src/Event/Processor/CustomerLogoutProcessor.php
@@ -5,15 +5,12 @@ declare(strict_types=1);
 namespace Synerise\SyliusIntegrationPlugin\Event\Processor;
 
 use Sylius\Component\Core\Model\CustomerInterface;
-use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Synerise\Api\V4\Models\Client;
-use Synerise\Api\V4\Models\LoggedOutEvent;
 use Synerise\Sdk\Api\RequestBody\Events\LoggedOutBuilder;
 use Synerise\Sdk\Exception\NotFoundException;
 use Synerise\Sdk\Tracking\IdentityManager;
 use Synerise\SyliusIntegrationPlugin\Api\RequestMapper\Event\CustomerToLoggedOutEvent;
 use Synerise\SyliusIntegrationPlugin\Entity\ChannelConfigurationFactory;
-use Synerise\SyliusIntegrationPlugin\Event\Model\BeforeLogoutRequestEvent;
 use Synerise\SyliusIntegrationPlugin\Event\Handler\EventHandlerResolver;
 use Webmozart\Assert\Assert;
 
--- src/Event/Processor/CustomerRegisterProcessor.php
+++ src/Event/Processor/CustomerRegisterProcessor.php
@@ -5,24 +5,22 @@ declare(strict_types=1);
 namespace Synerise\SyliusIntegrationPlugin\Event\Processor;
 
 use Sylius\Component\Core\Model\CustomerInterface;
-use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Synerise\Api\V4\Models\Client;
-use Synerise\Api\V4\Models\RegisteredEvent;
 use Synerise\Sdk\Api\RequestBody\Events\RegisteredBuilder;
 use Synerise\Sdk\Exception\NotFoundException;
 use Synerise\Sdk\Tracking\IdentityManager;
+use Synerise\SyliusIntegrationPlugin\Api\RequestMapper\Event\CustomerToRegisteredEvent;
 use Synerise\SyliusIntegrationPlugin\Entity\ChannelConfigurationFactory;
-use Synerise\SyliusIntegrationPlugin\Event\Model\BeforeRegisterRequestEvent;
 use Synerise\SyliusIntegrationPlugin\Event\Handler\EventHandlerResolver;
 use Webmozart\Assert\Assert;
 
 class CustomerRegisterProcessor implements CustomerProcessorInterface
 {
     public function __construct(
+        private CustomerToRegisteredEvent $mapper,
         private ChannelConfigurationFactory $configurationFactory,
         private IdentityManager $identityManager,
         private EventHandlerResolver $eventHandlerResolver,
-        private EventDispatcherInterface $eventDispatcher
     ) {
     }
 
@@ -38,30 +36,16 @@ class CustomerRegisterProcessor implements CustomerProcessorInterface
 
         Assert::NotNull($configuration->getChannel());
 
-        $this->eventHandlerResolver->get($type)->processEvent(
-            RegisteredBuilder::ACTION,
-            $this->prepareCustomerRegisteredRequestBody($customer),
-            $configuration->getChannel()->getId()
-        );
-    }
-
-    private function prepareCustomerRegisteredRequestBody(CustomerInterface $customer): RegisteredEvent
-    {
-        $client = new Client();
-
         try {
             $client = $this->identityManager->getClient();
         } catch (NotFoundException $exception) {
+            $client = new Client();
         }
 
-        $client->setEmail($customer->getEmail());
-        $client->setCustomId((string)$customer->getId());
-
-        $registeredEvent = RegisteredBuilder::initialize($client)->build();
-
-        $event = new BeforeRegisterRequestEvent($registeredEvent, $customer);
-        $this->eventDispatcher->dispatch($event, BeforeRegisterRequestEvent::NAME);
-
-        return $event->getRegisteredEvent();
+        $this->eventHandlerResolver->get($type)->processEvent(
+            RegisteredBuilder::ACTION,
+            $this->mapper->prepare($customer, $client),
+            $configuration->getChannel()->getId()
+        );
     }
 }
