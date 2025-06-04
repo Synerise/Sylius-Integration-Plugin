313b4f893ee7311a0194b5dbde5ed7f7babaac3a
Bartosz Sieczka <bartosz.sieczka@synerise.com>
2025-06-04 06:56:52 +0200

    Logger

--- src/Api/ClientBuilderFactory.php
+++ src/Api/ClientBuilderFactory.php
@@ -2,13 +2,42 @@
 
 namespace Synerise\SyliusIntegrationPlugin\Api;
 
+use Loguzz\Middleware\LogMiddleware;
+use Microsoft\Kiota\Abstractions\RequestAdapter;
+use Psr\Log\LoggerInterface;
+use Synerise\Sdk\Api\Authentication\AuthenticationProviderFactory;
 use Synerise\Sdk\Api\ClientBuilder;
 use Synerise\Sdk\Api\Config;
+use Synerise\Sdk\Guzzle\RequestAdapterFactory;
+use Synerise\SyliusIntegrationPlugin\Loguzz\Formatter\RequestCurlSanitizedFormatter;
 
-class ClientBuilderFactory
+class ClientBuilderFactory implements \Psr\Log\LoggerAwareInterface
 {
-    public function create(Config $config): ClientBuilder
+    public function __construct(
+        private LoggerInterface $syneriseLogger
+    ){}
+
+    public function create(Config $config, ?RequestAdapter $requestAdapter = null): ClientBuilder
     {
-        return new ClientBuilder($config);
+        $middlewares = [
+            new LogMiddleware(
+                $this->syneriseLogger,
+                ['request_formatter' => new RequestCurlSanitizedFormatter()]
+            )
+        ];
+
+        $authenticationProviderFactory = new AuthenticationProviderFactory($config);
+        $requestAdapterFactory = new RequestAdapterFactory($config);
+        $requestAdapter = $requestAdapterFactory->create(
+            $authenticationProviderFactory->create(),
+            $middlewares
+        );
+
+        return new ClientBuilder($config, $requestAdapter);
+    }
+
+    public function setLogger(LoggerInterface $logger): void
+    {
+        $this->syneriseLogger = $logger;
     }
 }
