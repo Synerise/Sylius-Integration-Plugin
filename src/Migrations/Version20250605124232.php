658ae8cee6eef09584c7b5b9e69de30db55cc6fb
Bartosz Sieczka <bartosz.sieczka@synerise.com>
2025-06-09 13:20:24 +0200

    Migration

--- src/Migrations/Version20250605124232.php
+++ src/Migrations/Version20250605124232.php
@@ -5,12 +5,12 @@ declare(strict_types=1);
 namespace Synerise\SyliusIntegrationPlugin\Migrations;
 
 use Doctrine\DBAL\Schema\Schema;
-use Doctrine\Migrations\AbstractMigration;
+use Sylius\Bundle\CoreBundle\Doctrine\Migrations\AbstractMigration;
 
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250527124734 extends AbstractMigration
+final class Version20250605124232 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -21,16 +21,16 @@ final class Version20250527124734 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql(<<<'SQL'
-            CREATE TABLE synerise_channel_configuration (id INT AUTO_INCREMENT NOT NULL, channel_id INT DEFAULT NULL, workspace_id INT DEFAULT NULL, tracking_code VARCHAR(255) DEFAULT NULL, tracking_enabled TINYINT(1) NOT NULL, cookie_domain VARCHAR(255) DEFAULT NULL, custom_page_visit TINYINT(1) NOT NULL, virtual_page TINYINT(1) NOT NULL, opengraph_enabled TINYINT(1) NOT NULL, events JSON DEFAULT NULL, queue_events JSON DEFAULT NULL, UNIQUE INDEX UNIQ_8BF5FA0272F5A1AA (channel_id), INDEX IDX_8BF5FA0282D40A1F (workspace_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET UTF8 COLLATE `UTF8_unicode_ci` ENGINE = InnoDB
+            CREATE TABLE synerise_channel_configuration (id INT AUTO_INCREMENT NOT NULL, channel_id INT DEFAULT NULL, workspace_id INT DEFAULT NULL, tracking_code VARCHAR(255) DEFAULT NULL, tracking_enabled TINYINT(1) NOT NULL, cookie_domain VARCHAR(255) DEFAULT NULL, custom_page_visit TINYINT(1) NOT NULL, virtual_page TINYINT(1) NOT NULL, opengraph_enabled TINYINT(1) NOT NULL, events JSON DEFAULT NULL, queue_events JSON DEFAULT NULL, UNIQUE INDEX UNIQ_8BF5FA0272F5A1AA (channel_id), INDEX IDX_8BF5FA0282D40A1F (workspace_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
         SQL);
         $this->addSql(<<<'SQL'
-            CREATE TABLE synerise_synchronization (id INT AUTO_INCREMENT NOT NULL, channel_id INT NOT NULL, status VARCHAR(255) NOT NULL, type VARCHAR(255) NOT NULL, configuration_snapshot VARCHAR(255) NOT NULL, total INT DEFAULT NULL, sent INT DEFAULT NULL, created_at DATETIME NOT NULL COMMENT '(DC2Type:datetime_immutable)', updated_at DATETIME DEFAULT NULL COMMENT '(DC2Type:datetime_immutable)', INDEX IDX_A9B5B81A72F5A1AA (channel_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET UTF8 COLLATE `UTF8_unicode_ci` ENGINE = InnoDB
+            CREATE TABLE synerise_synchronization (id INT AUTO_INCREMENT NOT NULL, channel_id INT NOT NULL, status VARCHAR(255) NOT NULL, type VARCHAR(255) NOT NULL, configuration_snapshot VARCHAR(255) NOT NULL, total INT DEFAULT NULL, sent INT DEFAULT NULL, created_at DATETIME NOT NULL COMMENT '(DC2Type:datetime_immutable)', updated_at DATETIME DEFAULT NULL COMMENT '(DC2Type:datetime_immutable)', INDEX IDX_A9B5B81A72F5A1AA (channel_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
         SQL);
         $this->addSql(<<<'SQL'
-            CREATE TABLE synerise_synchronization_config (id INT AUTO_INCREMENT NOT NULL, channel_id INT DEFAULT NULL, data_types JSON DEFAULT NULL, product_attributes JSON DEFAULT NULL, catalog_id INT DEFAULT NULL, UNIQUE INDEX UNIQ_F0CA4B8C72F5A1AA (channel_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET UTF8 COLLATE `UTF8_unicode_ci` ENGINE = InnoDB
+            CREATE TABLE synerise_synchronization_config (id INT AUTO_INCREMENT NOT NULL, channel_id INT DEFAULT NULL, data_types JSON DEFAULT NULL, product_attributes JSON DEFAULT NULL, catalog_id INT DEFAULT NULL, product_attribute_value VARCHAR(10) NOT NULL, UNIQUE INDEX UNIQ_F0CA4B8C72F5A1AA (channel_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
         SQL);
         $this->addSql(<<<'SQL'
-            CREATE TABLE synerise_workspace (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) DEFAULT NULL, apiKey BINARY(16) NOT NULL COMMENT '(DC2Type:uuid)', apiGuid CHAR(36) DEFAULT NULL COMMENT '(DC2Type:guid)', authenticationMethod VARCHAR(10) NOT NULL, environment VARCHAR(10) NOT NULL, permissions LONGTEXT DEFAULT NULL COMMENT '(DC2Type:array)', PRIMARY KEY(id)) DEFAULT CHARACTER SET UTF8 COLLATE `UTF8_unicode_ci` ENGINE = InnoDB
+            CREATE TABLE synerise_workspace (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) DEFAULT NULL, apiKey BINARY(16) NOT NULL COMMENT '(DC2Type:uuid)', apiGuid CHAR(36) DEFAULT NULL COMMENT '(DC2Type:guid)', authenticationMethod VARCHAR(10) NOT NULL, environment VARCHAR(10) NOT NULL, permissions LONGTEXT DEFAULT NULL COMMENT '(DC2Type:array)', PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
         SQL);
         $this->addSql(<<<'SQL'
             ALTER TABLE synerise_channel_configuration ADD CONSTRAINT FK_8BF5FA0272F5A1AA FOREIGN KEY (channel_id) REFERENCES sylius_channel (id)
