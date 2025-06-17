19f18b079879411a1a6ed8ed7e4575e12a24f820
kamilkanik <kamil.kanik@icloud.com>
2025-06-17 10:16:13 +0200

    channel configuration

--- src/Form/Type/ChannelConfigurationType.php
+++ src/Form/Type/ChannelConfigurationType.php
@@ -7,6 +7,8 @@ use Sylius\Bundle\ResourceBundle\Form\Type\AbstractResourceType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 
 final class ChannelConfigurationType extends AbstractResourceType
 {
@@ -20,36 +22,74 @@ final class ChannelConfigurationType extends AbstractResourceType
                 'label' => 'synerise_integration.ui.workspace'
             ])
             ->add('trackingEnabled', CheckboxType::class, [
-                'label' => 'synerise_integration.ui.tracking_enabled',
-                'required' => false
-            ])
-            ->add('cookieDomain', TextType::class, [
-                'label' => 'synerise_integration.ui.cookie_domain',
-                'required' => false
-            ])
-            ->add('customPageVisit', CheckboxType::class, [
-                'label' => 'synerise_integration.ui.custom_page_visit',
-                'required' => false
-            ])
-            ->add('virtualPage', CheckboxType::class, [
-                'label' => 'synerise_integration.ui.virtual_page',
+                'label' => 'synerise_integration.ui.',
                 'required' => false
             ])
             ->add('opengraphEnabled', CheckboxType::class, [
-                'label' => 'synerise_integration.ui.opengraph_enabled',
+                'label' => 'Render OG tags from page visit events',
+                'required' => false
+            ])
+            ->add('virtualPage', CheckboxType::class, [
+                'label' => 'Dynamic content for PWA, SPA sites',
+                'help' => 'Enabling this option lets you display Synerise dynamic content on single page or progressive web application.',
+                'required' => false
+            ])
+            ->add('cookieDomainEnabled', CheckboxType::class, [
+                'label' => 'Override cookie domain',
+                'help' => 'Declare a specific domain for cookies if several subdomains share a single workspace.',
+                'required' => false,
+                'mapped' => false,
+            ])
+            ->add('cookieDomain', TextType::class, [
+                'label' => 'Declare a specific domain/root domain',
+                'attr'=> [
+                    'placeholder' => 'domain.com',
+                ],
+                'required' => false
+            ])
+            ->add('customPageVisit', CheckboxType::class, [
+                'label' => 'Custom page visit implementation ',
+                'help' => 'Recommended for PWA themes. Enabling this option requires code changes. For more information check the documentation.',
                 'required' => false
             ])
             ->add('events', EventChoiceType::class, [
                 'label' => 'synerise_integration.ui.events',
+                'choice_translation_domain' => true,
                 'multiple' => true,
-                'required' => false
+                'required' => false,
+                'attr' => [
+                    'data-controller' => 'multiselect'
+                ]
             ])
             ->add('queueEvents', EventChoiceType::class, [
                 'label' => 'synerise_integration.ui.queue_events',
+                'choice_translation_domain' => true,
                 'multiple' => true,
-                'required' => false
+                'required' => false,
+                'attr' => [
+                    'data-controller' => 'multiselect'
+                ]
             ])
         ;
+
+        $builder->addEventListener(FormEvents::PRE_SUBMIT, function (FormEvent $event) {
+            $data = $event->getData();
+
+            if (isset($data['cookieDomainEnabled']) && !$data['cookieDomainEnabled']) {
+                $data['cookieDomain'] = null;
+                $event->setData($data);
+            }
+        });
+
+        $builder->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) {
+            $data = $event->getData();
+            $form = $event->getForm();
+
+            if ($data) {
+                $form->get('cookieDomainEnabled')->setData($data->getCookieDomain() !== null);
+            }
+        });
+
     }
 
     public function getBlockPrefix(): string
