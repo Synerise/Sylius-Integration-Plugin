0265961593cb04d1b500e4b4fcb96a4b36dee008
Bartosz Sieczka <bartosz.sieczka@synerise.com>
2025-06-17 16:51:04 +0200

    Fix enum labels

--- src/Model/Workspace/Environment.php
+++ src/Model/Workspace/Environment.php
@@ -3,16 +3,25 @@
 namespace Synerise\SyliusIntegrationPlugin\Model\Workspace;
 
 
+use Symfony\Contracts\Translation\TranslatableInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
 use Synerise\Sdk\Model\EnumTrait;
 use Synerise\Sdk\Model\EnvironmentInterface;
 use Synerise\Sdk\Model\EnvironmentTrait;
 
-enum Environment: string implements EnvironmentInterface
+enum Environment: string implements EnvironmentInterface, TranslatableInterface
 {
     use EnvironmentTrait;
     use EnumTrait;
 
     case Azure = self::AZURE_VALUE;
 
+    case AzureUs = self::AZURE_US_VALUE;
+
     case GCP = self::GCP_VALUE;
+
+    public function trans(TranslatorInterface $translator, ?string $locale = null): string
+    {
+        return $this->label();
+    }
 }
