fae193b88ea2b5891c77e07d4c02fde02de5abdd
Bartosz Sieczka <bartosz.sieczka@synerise.com>
2025-10-15 18:09:57 +0200

    ProductDataProvider with product repository

--- src/Synchronization/DataProvider/ProductDataProvider.php
+++ src/Synchronization/DataProvider/ProductDataProvider.php
@@ -4,26 +4,25 @@ declare(strict_types=1);
 
 namespace Synerise\SyliusIntegrationPlugin\Synchronization\DataProvider;
 
-use Doctrine\ORM\EntityManagerInterface;
 use Sylius\Component\Channel\Model\ChannelInterface;
 use Sylius\Component\Core\Model\Product;
+use Sylius\Component\Core\Repository\ProductRepositoryInterface;
 use Sylius\Resource\Model\ResourceInterface;
 
 class ProductDataProvider implements DataProviderInterface
 {
     public function __construct(
-        private EntityManagerInterface $entityManager,
+        private ProductRepositoryInterface $productRepository,
     ) {
     }
 
     public function getIds(ChannelInterface $channel): iterable
     {
-        $queryBuilder = $this->entityManager->createQueryBuilder();
+        $queryBuilder = $this->productRepository->createQueryBuilder('o');
         $queryBuilder->select('o.id')
-            ->from(Product::class, 'o')
             ->andWhere(':channel MEMBER OF o.channels')
             ->setParameter('channel', $channel);
-
+        
         return $queryBuilder->getQuery()->toIterable();
     }
 
@@ -32,6 +31,6 @@ class ProductDataProvider implements DataProviderInterface
      */
     public function getEntity(int $id): ?ResourceInterface
     {
-        return $this->entityManager->getRepository(Product::class)->find($id);
+        return $this->productRepository->find($id);
     }
 }
